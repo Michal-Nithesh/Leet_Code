225715bca3f6c2cd72bf5b76c0e16a1836ed4bf8
Michal Nithesh <151201789+Michal-Nithesh@users.noreply.github.com>
2025-01-10 22:01:22 +0530

    Update AllowOneFunctionCalls.js

--- Sum - 007/AllowOneFunctionCalls.js
+++ Sum - 007/AllowOneFunctionCalls.js
@@ -12,10 +12,9 @@ var once = funnction(fn){
   }
 }
 
-/**
- * let fn = (a,b,c) => (a + b + c)
- * let onceFn = once(fn)
- *
- * onceFn(1,2,3); // 6
- * onceFn(2,3,6); // returns undefined without calling fn
- */
+
+  let fn = (a,b,c) => (a + b + c)
+  let onceFn = once(fn)
+
+  onceFn(1,2,3); // 6
+  onceFn(2,3,6); // returns undefined without calling fn
