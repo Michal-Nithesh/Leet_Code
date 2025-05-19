cb4b5d142295194150d19ea88c562922acf36066
Michal Nithesh <151201789+Michal-Nithesh@users.noreply.github.com>
2025-05-19 15:32:14 +0530

    Create CountDigitsThatDivideANumber.java

---
+++ Sum - 013/CountDigitsThatDivideANumber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countDigits(int num) {
+        int orgNum = num;
+        int count = 0;
+        while(num > 0){
+            int digit = num % 10;
+            if(digit != 0 && orgNum % digit == 0){
+                count++;
+            }
+            num = num / 10;
+        }
+        return count;
+    }
+}
