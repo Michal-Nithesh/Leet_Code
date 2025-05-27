90f10e59682dc222ccbf3427b834a2e1b2a839f0
Michal Nithesh <151201789+Michal-Nithesh@users.noreply.github.com>
2025-05-27 19:34:33 +0530

    Create KidsWithTheGreatestNum.java

---
+++ Sum - 021/KidsWithTheGreatestNum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int n = candies.length;
+        int maxValue = candies[0];
+        List<Boolean> results = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            if (candies[i] > maxValue){
+                maxValue = candies[i];
+            }
+        }
+        for(int i = 0; i < n; i++){
+            if(candies[i] + extraCandies >= maxValue){
+                results.add(true);
+            } else {
+                results.add(false);
+            }
+        }
+        return results;
+    }
+}
